d4d0b7b48769aaf93187aeafdf57d0ca331a289a
zsmoore <zsmoore@zachary-moore.com>
2022-04-11 11:42:22 -0400

    remove processor

--- sound-experiments/processor.js
+++
@@ -1,8 +0,0 @@
-class Processor extends AudioWorkletProcessor {
-  process (inputs, outputs, parameters) {
-    // this is awful but im not setting up import exports
-    dataArray = inputs[0];
-  }
-}
-
-registerProcessor('processor', Processor);
